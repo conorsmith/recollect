f1e9bf8c2ce257818b9db00648d9897eb77bcc1b
Conor Smith <conorjbsmith@gmail.com>
2020-09-17 21:54:09 +0100

    Improve look of landing page

--- src/Infrastructure/Templates/LandingPage.php
+++ src/Infrastructure/Templates/LandingPage.php
@@ -9,31 +9,51 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <title>Recollect</title>
+
+    <style>
+        body {
+            --stripe: #f8f8f8;
+            --bg: #fcfcfc;
+
+            background: linear-gradient(135deg, var(--bg) 25%, transparent 25%) -50px 0,
+            linear-gradient(225deg, var(--bg) 25%, transparent 25%) -50px 0,
+            linear-gradient(315deg, var(--bg) 25%, transparent 25%),
+            linear-gradient(45deg, var(--bg) 25%, transparent 25%);
+            background-size: 100px 100px;
+            background-color: var(--stripe);
+        }
+    </style>
+
 </head>
 <body>
 
-<div class="container">
+<div class="container" style="font-family: 'Hind', sans-serif;">
 
-    <h1 style="text-align: center; margin-top: 0.4rem; margin-bottom: 1rem;">Recollect</h1>
+    <div style="max-width: 20rem; margin: 0 auto;">
 
-    <div class="card" style="margin-bottom: 1rem; text-align: center;">
-        <div class="card-body">
-            <form method="POST" action="/open-table">
-                <button type="submit" class="btn btn-primary">Create New Game</button>
-            </form>
+        <h1 style="text-align: center; margin: 1rem auto; font-weight: 100; text-transform: uppercase; letter-spacing: 0.1rem;"><span style="color: #666;">Re</span>collect</h1>
+
+        <div class="card" style="text-align: center;">
+            <div class="card-body">
+                <form method="POST" action="/join-table" autocomplete="off">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="code" maxlength="4" placeholder="Join Code">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Play</button>
+                </form>
+            </div>
         </div>
-    </div>
 
-    <div class="card" style="text-align: center;">
-        <div class="card-body">
-            <form method="POST" action="/join-table">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Join Code</label>
-                    <input type="text" class="form-control" name="code" maxlength="4">
-                </div>
-                <button type="submit" class="btn btn-primary">Join Game</button>
-            </form>
+        <div style="text-align: center; font-weight: 100; margin: 1rem auto;">OR</div>
+
+        <div class="card">
+            <div class="card-body">
+                <form method="POST" action="/open-table">
+                    <button type="submit" class="btn btn-primary btn-block">Create New Game</button>
+                </form>
+            </div>
         </div>
+
     </div>
 
 </div>
